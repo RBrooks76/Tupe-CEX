982e66dcc436d045c4022711e8221db1097ba6d5
woody0706 <99725957+woody0706@users.noreply.github.com>
2022-06-21 16:51:18 +1000

    Update index.js

--- pages/market/index.js
+++ pages/market/index.js
@@ -325,14 +325,14 @@ const MarketScreen = ({coins}) => {
     const url = 'wss://stream.binance.com:9443/stream?streams=!ticker@arr@3000ms'// + string + '@kline_1m';
     const isBrowser = typeof window !== "undefined";
     const ws = isBrowser ? new WebSocket(url) : null;
-    const msg = {
-      method: 'SUBSCRIBE',
-      params: [],
-      id: 1
-    }
+    // const msg = {
+    //   method: 'SUBSCRIBE',
+    //   params: [],
+    //   id: 1
+    // }
     if (!isNil(ws)) {
       ws.onopen = (event) => {
-        ws.send(JSON.stringify(msg));
+        // ws.send(JSON.stringify(msg));
       };
       ws.onmessage = function (event) {
         var data = JSON.parse(event.data);
